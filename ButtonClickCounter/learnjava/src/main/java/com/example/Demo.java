e4dfe4ae6eed7e35e2421eae3689edb7b6706088
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-09 10:31:43 -0300

    [Java Lesson]
     - Changed the message

--- ButtonClickCounter/learnjava/src/main/java/com/example/Demo.java
+++ ButtonClickCounter/learnjava/src/main/java/com/example/Demo.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, world! Now!");
     }
 }
