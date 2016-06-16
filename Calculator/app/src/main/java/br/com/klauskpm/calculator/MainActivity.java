0384bd19acd76f5427d489d59973b6575241f2af
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-16 19:18:28 -0300

    [Calculator]
     - Button action;
     - Data validation;

--- Calculator/app/src/main/java/br/com/klauskpm/calculator/MainActivity.java
+++ Calculator/app/src/main/java/br/com/klauskpm/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.klauskpm.calculator;
 
 import android.os.Bundle;
+import android.renderscript.Double2;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnDivision;
+    private Button btnMultiplication;
+
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+
+        textResult = (TextView) findViewById(R.id.txtResult);
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = getOper(operand1);
+                double oper2 = getOper(operand2);
+
+                double theResult = oper1 + oper2;
+
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = getOper(operand1);
+                double oper2 = getOper(operand2);
+
+                double theResult = oper1 - oper2;
+
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = getOper(operand1);
+                double oper2 = getOper(operand2);
+
+                if (oper2 == 0f)
+                    oper2 = 1f;
+
+                double theResult = oper1 / oper2;
+
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = getOper(operand1);
+                double oper2 = getOper(operand2);
+
+                double theResult = oper1 * oper2;
+
+                textResult.setText(Double.toString(theResult));
+            }
+        });
+    }
+
+    private double getOper(EditText operand) {
+        String oper = operand.getText().toString();
+
+        if (oper.isEmpty())
+            return 0f;
+        else
+            return Double.parseDouble(oper);
     }
 
     @Override
