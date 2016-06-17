4f706c937a5a40c1538c2efeac8a06100798cfa5
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-17 09:36:18 -0300

    [Top 10 Download]
     - XML Download;

--- Top10Download/app/src/main/java/br/com/klauskpm/top10download/MainActivity.java
+++ Top10Download/app/src/main/java/br/com/klauskpm/top10download/MainActivity.java
@@ -6,9 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,5 +49,47 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error downloading");
+            }
+
+            return mFileContents;
+        }
+
+        private String downloadXMLFile(String urlPath) {
+            StringBuilder tempbuffer = new StringBuilder();
+
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", "The response code was " + response);
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+                char[] inputBuffer = new char[500];
+
+                while (true) {
+                    charRead = isr.read(inputBuffer);
+                    if (charRead<=0) {
+                        break;
+                    }
+                    tempbuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+                }
+            } catch(IOException e) {
+                Log.d("DownloadData", "IO Exception readin data: " + e.getMessage());
+            }
+
+            return tempbuffer.toString();
+        }
+    }
 }
