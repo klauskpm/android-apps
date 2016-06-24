34b9aea340b31c46e0d22675de6b531564e6708a
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-24 10:14:42 -0300

    [Flickr Browser]
     - Json data parsing(1);

---
+++ FlickrBrowser/app/src/main/java/br/com/klauskpm/flickrbrowser/GetFlickrJsonData.java
@@ -0,0 +1,41 @@
+package br.com.klauskpm.flickrbrowser;
+
+import android.net.Uri;
+
+import java.util.List;
+
+/**
+ * Created by klaus.machado on 24/06/2016.
+ */
+public class GetFlickrJsonData extends GetRawData {
+
+    private String TAG = GetFlickrJsonData.class.getSimpleName();
+    private List<Photo> mPhotos;
+    private Uri mDestinationUri;
+
+    public GetFlickrJsonData(String searchCriteria, boolean matchAll) {
+        super(null);
+        createAndUpdateUri(searchCriteria, matchAll);
+    }
+
+    private boolean createAndUpdateUri(String searchCriteria, boolean matchAll) {
+        final String FLICKR_API_BASE_URL = "https://api.flickr.com/services/feeds/photos_public.gne";
+        final String TAGS_PARAM = "tags";
+        final String TAGMODE_PARAM = "tagmode";
+        final String FORMAT_PARAM = "format";
+        final String NO_JSON_CALLBACK_PARAM = "nojsoncallback";
+
+        mDestinationUri = Uri.parse(FLICKR_API_BASE_URL).buildUpon()
+                .appendQueryParameter(TAGS_PARAM, searchCriteria)
+                .appendQueryParameter(TAGMODE_PARAM, matchAll ? "all" : "any")
+                .appendQueryParameter(FORMAT_PARAM, "json")
+                .appendQueryParameter(NO_JSON_CALLBACK_PARAM, "1")
+                .build();
+
+        return mDestinationUri != null;
+    }
+
+    public class DownloadJsonData extends DownloadRawData {
+        
+    }
+}
---
+++ FlickrBrowser/app/src/main/java/br/com/klauskpm/flickrbrowser/Photo.java
@@ -0,0 +1,58 @@
+package br.com.klauskpm.flickrbrowser;
+
+/**
+ * Created by klaus.machado on 24/06/2016.
+ */
+public class Photo {
+    private String mTitle;
+    private String mAuthor;
+    private String mAuthorId;
+    private String mLink;
+    private String mTags;
+    private String mImage;
+
+    public Photo(String mTitle, String mAuthor, String mAuthorId, String mLink, String mTags, String mImage) {
+        this.mTitle = mTitle;
+        this.mAuthor = mAuthor;
+        this.mAuthorId = mAuthorId;
+        this.mLink = mLink;
+        this.mTags = mTags;
+        this.mImage = mImage;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getAuthorId() {
+        return mAuthorId;
+    }
+
+    public String getLink() {
+        return mLink;
+    }
+
+    public String getTags() {
+        return mTags;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTags='" + mTags + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
+}
