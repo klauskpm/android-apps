faceae32e5b89a01cf8267a2b67eb6f901cf81a9
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-29 09:06:12 -0300

    [Flickr Browser]
     - Creating the basic GUI(1);
     - Picasso Image Library;

---
+++ FlickrBrowser/app/src/main/java/br/com/klauskpm/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,24 @@
+package br.com.klauskpm.flickrbrowser;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by klaus.machado on 29/06/2016.
+ */
+public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrImageViewHolder> {
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
